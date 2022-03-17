6fb5dc51e843e5f622c84cab86bcee00e1ac099f
harry <git@hskr.xyz>
2022-03-17 13:22:32 +0000

    only send to localhost

--- car-api-docker/frontend/files/js/main.js
+++ car-api-docker/frontend/files/js/main.js
@@ -8,13 +8,11 @@ const testMain = async () => {
 });
 
   const opts = {
-        "Access-Control-Allow-Origin": "*",
-        'Content-Type': 'application/json',
   }
 
   const http = new simpleFETCH;
 
-  await http.get("http://car-api:8080/readItem/8", opts)
+  await http.get("http://localhost:8080/readItem/8", opts)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
