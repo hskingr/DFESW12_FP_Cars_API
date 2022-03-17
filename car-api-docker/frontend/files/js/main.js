dc9537788f95d5e71e072730b9bc85db77367c25
harry <git@hskr.xyz>
2022-03-17 12:38:31 +0000

    added frontend files

--- car-api-docker/frontend/files/js/main.js
+++ car-api-docker/frontend/files/js/main.js
@@ -1,6 +1,12 @@
 const testMain = async () => {
   console.log('hi from main')
 
+  ping('http://localhost').then(function(delta) {
+    console.log('Ping time was ' + String(delta) + ' ms');
+}).catch(function(err) {
+    console.error('Could not ping remote URL', err);
+});
+
   const opts = {
         "Access-Control-Allow-Origin": "*",
         'Content-Type': 'application/json',
@@ -8,10 +14,12 @@ const testMain = async () => {
 
   const http = new simpleFETCH;
 
-  await http.get("http://127.0.0.1:8080/readItem/1", opts)
+  await http.get("http://localhost:8080/readItem/1", opts)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
 }
+
+
 
 testMain()
